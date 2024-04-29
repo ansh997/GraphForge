7aba4e145e036d5ba5b34f5b223d2fff22475d38
ansh997 <palhimanshu997@gmail.com>
2024-04-29 10:38:42 +0530

    changed structure

---
+++ graphqa/__init__.py
@@ -0,0 +1,11 @@
+# from .name_dictionaries import *
+# from .graph_text_encoder import *
+# from .graph_task import *
+# from .graph_task_utils import *
+# from .graph_task_generator import *
+# from .graph_generator import *
+# from .graph_generator_utils import *
+
+__all__ = ['name_dictionaries', 
+'graph_text_encoder', 'graph_task', 'graph_task_utils', 'graph_task_generator', 'graph_generator', 'graph_generator_utils',
+'graph_task_generator', 'graph_generator', 'graph_generator_utils']
