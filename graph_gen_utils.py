4919a44c7a61269871a7d937eeaff3bab9425342
ansh997 <palhimanshu997@gmail.com>
2024-04-30 21:29:58 +0530

    added data

--- graph_gen_utils.py
+++ graph_gen_utils.py
@@ -215,7 +215,7 @@ def gen_save_graphs(graph_type: str, nodes: int=None, directed: bool=False, save
         save_path is None else os.path.join(save_path, 'data', graph_type)
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    filename = f"{graph_type}_{nodes}_{dir_flag}_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.gpickle" if filename is None else filename
+    filename = f"{graph_type}_{nodes}_{dir_flag}_{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.gpickle" if filename is None else filename
     save_path = os.path.join(save_path, filename)
     nx.write_gpickle(G, save_path)
     print(f"Saved {graph_type} graph to {save_path}")
